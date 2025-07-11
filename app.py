5487a95912e517b78c2a73207c41dfde2a365fb4
BMateusSs <bmdss0810@gmail.com>
2025-07-10 22:29:09 -0300

    Inicio do app

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+from get_top5_album_home import get_top5_album_home
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/top5_album_home', methods=['GET'])
 def top5_album_home():
-    return 'Home'
+    albuns = get_top5_album_home(1)
+
+    dados = []
+    for album in albuns:
+        dados.append({
+            'album_name': album[0],
+            'artist': album[1],
+            'rank_position': album[2],
+            'last_week': album[3],
+            'total_weeks': album[4],
+            'album_cover': album[5]
+        })
+    
+    return jsonify(dados)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
